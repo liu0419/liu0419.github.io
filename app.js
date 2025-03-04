6cad96fc9e73f44b3bc39b79dd7aecfe3aaf860f
劉丞晏 <h0985316554@gmail.com>
2025-03-04 18:15:42 +0800

    use ssl port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const connectDB = require('./mongo/db.js');
 const certiRouter = require('./routes/certi.js');
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
 connectDB()
